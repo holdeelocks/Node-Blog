a8235b783d0049f4b3b5a1070793fb2f995a2645
Holden B <holden.bucher.92@gmail.com>
2019-01-10 17:16:13 -0600

    add tag routes to postRoutes just for funsies

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -3,10 +3,61 @@ const router = express.Router();
 
 const posts = require(".././data/helpers/postDb");
 const users = require(".././data/helpers/userDb");
+const tags = require(".././data/helpers/tagDb");
 
 const errorMaker = (code, message, res) => {
   res.status(code).json({ error: message });
 };
+
+router.get("/tags/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const tag = await tags.get(id);
+    return tag ? res.status(200).json(tag) : errorMaker(404, "no exist", res);
+  } catch (err) {
+    errorMaker(500, "Can't reach server", res);
+  }
+});
+
+router.get("/tags", async (req, res) => {
+  try {
+    const tagList = await tags.get();
+    res.status(200).json(tagList);
+  } catch (err) {
+    errorMaker(500, "Can't reach server", res);
+  }
+});
+
+router.post("/tags", async (req, res) => {
+  const tag = req.body;
+  try {
+    const success = await tags.insert(tag);
+    res.status(201).json(success);
+  } catch (err) {
+    errorMaker(500, "Can't reach server", res);
+  }
+});
+
+router.put("/tags/:id", async (req, res) => {
+  const { id } = req.params;
+  const updated = req.body;
+  try {
+    const success = await tags.update(id, updated);
+    return success ? res.status(201).json(success) : errorMaker(404, "no exist", res);
+  } catch (err) {
+    errorMaker(500, "Can't reach server", res);
+  }
+});
+
+router.delete("/tags/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const success = await tags.remove(id);
+    return success ? res.status(200).json(success) : errorMaker(404, "no exist", res);
+  } catch (err) {
+    errorMaker(500, "no server", res);
+  }
+});
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
